6f1d6f6f49abe7fe6fc3e6a408e6cd93d90273a9
Lucas Cabral <cabralz18@outlook.com>
2023-07-30 20:39:39 -0300

    Header responsive

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,22 +1,28 @@
-    var slide_podcast = new Swiper(".slide-podcast", {
-      slidesPerView: 4,
-      spaceBetween: 32,
-      pagination: {
-        el: ".s-podcast .top .swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".s-podcast .top .btn-next",
-        prevEl: ".s-podcast .top .btn-prev",
-      },
-      speed: 600,
+var slide_podcast = new Swiper(".slide-podcast", {
+  slidesPerView: 4,
+  spaceBetween: 32,
+  pagination: {
+  el: ".s-podcast .top .swiper-pagination",
+  clickable: true,
+  },
+  navigation: {
+  nextEl: ".s-podcast .top .btn-next",
+  prevEl: ".s-podcast .top .btn-prev",
+  },
+  speed: 600,
+  });
+
+const btnScrollTop = document.getElementById('js-btn-scroll-top');
+
+  btnScrollTop.addEventListener('click', () => {
+    window.scrollTo ({
+      top: 0,
+      behavior: "smooth"
     });
+  })
 
-    const btnScrollTop = document.getElementById('js-btn-scroll-top');
+const btnMobile = document.getElementById('js-btn-mobile');
 
-    btnScrollTop.addEventListener('click', () => {
-        window.scrollTo ({
-            top: 0,
-            behavior: "smooth"
-        });
-    })
+btnMobile.addEventListener('click', () => {
+  btnMobile.classList.toggle('is-active');
+})
